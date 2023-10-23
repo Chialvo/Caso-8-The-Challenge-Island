a41ef304ce885ae03e6f21319e30f388e2f39cf1
Chialvo <lautarochialvo2005@gmail.com>
2023-10-23 11:52:31 -0300

    login-prueba

--- TheChallengeIsland/TCI/urls.py
+++ TheChallengeIsland/TCI/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.prueba, name="prueba"),
-    path("home/", views.home, name="home"),
+    path("", views.home, name="home"),
     path('login/', views.login, name='login'),
     path("pruebaequipos/",views.lista_equipos, name="equipos"),
     path('prueba/', views.prueba, name='prueba'),
     path('temporadas/', views.temporadas, name='temporadas'),
     path('equipos/', views.equipos, name='equipos'),
     path('participantes/', views.participantes, name='participantes'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- TheChallengeIsland/TCI/views.py
+++ TheChallengeIsland/TCI/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from .models import *
 from TCI.models import *
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -22,15 +23,16 @@ def lista_equipos(request):
         participantes = Participante.objects.filter(equipo=equipo)
         data.append({'equipo': equipo, 'participantes': participantes})
         return render(request, 'pruebaequipos.html', {'data': data})
+@login_required
 def temporadas(request):
     temporadas = Temporada.objects.all()
 
     return render(request, "temporadas.html", {'temporadas': temporadas})
-
+@login_required
 def equipos(request):
     equipos = Equipo.objects.all()
     return render(request, "equipos.html", {'equipos': equipos})
-
+@login_required
 def participantes(request):
     participantes = Participante.objects.all()
     return render(request, "participantes.html", {'participantes': participantes})
