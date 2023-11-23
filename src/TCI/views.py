1f192e2f3165340c290e47023800b77e4e291ba0
Augusto Guchea <aguchealezama@gmail.com>
2023-11-23 12:09:42 -0300

    Cambios xd

--- src/TCI/views.py
+++ src/TCI/views.py
@@ -4,41 +4,8 @@ from TCI.models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.db.models import Q
+from .funcionalidades import *
 
-
-def buscarTemporada( busqueda):
-    temporadas = Temporada.objects.all()
-    try:
-        temporada_relacionada = temporadas.filter(
-            Q(nombre__icontains=busqueda) |
-            Q(numero=busqueda) |
-            Q(puntos=busqueda)).distinct()
-    except ValueError:
-        temporada_relacionada = temporadas.filter(
-            Q(nombre__icontains=busqueda)).distinct()
-    temporada_no_relacionadas = temporadas.exclude(id__in=temporada_relacionada.values_list('id', flat=True))
-    resultado = list(temporada_relacionada) + list(temporada_no_relacionadas)
-    return resultado
-
-def buscarEquipo(busqueda):
-    equipos = Equipo.objects.all()
-    equipo_relacionado = equipos.filter(
-        Q(nombre__icontains=busqueda) | Q()).distinc()
-    equipo_no_relacionado = participantes.exclude(id__in=equipo_relacionado.values_list('id', flat=True))
-    resultado = list(equipo_relacionado) + list(equipo_no_relacionado)
-    return resultado
-
-def buscarParticipante(busqueda):
-    participantes = Participante.objects.all()
-    participante_relacionado = participantes.filter(
-        Q(nombre__icontains=busqueda) |
-        Q(apellido__icontains=busqueda) |
-        Q(apodo__icontains=busqueda) |
-        Q(pais__nombre__icontains=busqueda) |
-        Q()).distinct()
-    participantes_no_relacionado = participantes.exclude(id__in=participante_relacionado.values_list('id', flat=True))
-    resultado = list(participante_relacionado) + list(participantes_no_relacionado)
-    return resultado
 
 def home(request):
     busqueda = request.GET.get('buscador')
